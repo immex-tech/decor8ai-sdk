75df2e49f6b2e9991cb624d975b948503d51f5bf
Akhilesh Joshi <akhileshjoshi@gmail.com>
2024-03-11 17:00:57 -0700

    Fixed a bug where input image is not fetched correctly. Converted createFormData to async function that needs to be awaited if the file is being fetched.

--- js/decor8ai/index.js
+++ js/decor8ai/index.js
@@ -18,7 +18,7 @@ class Decor8AI {
                 'Content-Type': 'multipart/form-data'
             };
 
-            const formData = this.createFormData(inputImage);
+            const formData = await this.createFormData(inputImage);
 
             const response = await axios.post(`${this.baseUrl}/prime_the_room_walls`, formData, { headers });
             return response.data;
@@ -35,7 +35,7 @@ class Decor8AI {
                 'Content-Type': 'multipart/form-data'
             };
 
-            const formData = this.createFormData(inputImage);
+            const formData = await this.createFormData(inputImage);
             formData.append('room_type', roomType);
             formData.append('design_style', designStyle);
             formData.append('num_images', numImages);
@@ -59,7 +59,7 @@ class Decor8AI {
                 'Content-Type': 'multipart/form-data'
             };
 
-            const formData = this.createFormData(inputImage);
+            const formData = await this.createFormData(inputImage);
             formData.append('scale_factor', scaleFactor);
 
             const response = await axios.post(`${this.baseUrl}/upscale_image`, formData, { headers });
@@ -70,25 +70,28 @@ class Decor8AI {
         }
     }
 
-    createFormData(inputImage) {
+    async createFormData(inputImage) {
         let formData = new FormData();
-
         if (typeof inputImage === 'string') { // Path or URL
             if (inputImage.startsWith('http')) { // URL
-                axios.get(inputImage, { responseType: 'stream' })
-                    .then(response => {
-                        formData.append('input_image', response.data, { filename: 'input_image.jpg' });
-                    })
-                    .catch(error => console.error(error));
+                try {
+                    const response = await axios.get(inputImage, { responseType: 'arraybuffer' });
+                    const buffer = Buffer.from(response.data, 'binary');
+                    formData.append('input_image', buffer, 'input_image.jpg');
+                } catch (error) {
+                    console.error(error);
+                    throw new Error("Failed to load image from URL");
+                }
             } else { // File Path
                 formData.append('input_image', fs.createReadStream(inputImage), 'input_image.jpg');
             }
         } else if (Buffer.isBuffer(inputImage)) { // Binary Data
-            formData.append('input_image', inputImage, { filename: 'input_image.jpg' });
+            formData.append('input_image', inputImage, 'input_image.jpg');
         }
-
+    
         return formData;
     }
+    
 
     async generateImageCaptions(room_type, design_style, num_captions = 1) {
         try {
